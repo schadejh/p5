2e49167fabfce991bdf296aeb9f725d6458e9ecb
James H. Schader <schadejh@dukes.jmu.edu>
2022-04-01 23:23:00 -0400

    first commit

---
+++ p5.pl
@@ -0,0 +1,21 @@
+%
+% CS 430 P5 (Prolog 1)
+%
+% Name:
+%
+
+parent(_,_).
+
+step_parent(_,_).
+
+sibling(_,_).
+
+aunt_uncle(_,_).
+
+grandparent(_,_).
+
+ancestor(_,_).
+
+relative(_,_).
+
+in_law(_,_).
